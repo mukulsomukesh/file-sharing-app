5156d9ec04c91c20a9c1f0adb4def81bb6c29231
mukulsomukesh <mukulsomukesh@gmail.com>
2023-06-20 02:56:30 +0530

    add preview for all types of files

--- frontend/src/components/DisplayFiles/Preview.jsx
+++ frontend/src/components/DisplayFiles/Preview.jsx
@@ -1,35 +1,88 @@
-import { Box, Button, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react"
+import { Box, Button, IconButton, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
 import { BsEyeFill } from "react-icons/bs";
 
-export default function Preview({fileData}) {
+export default function Preview({ fileData }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const getFilePreview = () => {
+    const fileExtension = getFileExtension(fileData);
   
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    switch (fileExtension) {
+      case "jpg":
+      case "jpeg":
+      case "png":
+      case "gif":
+      case "bmp":
+      case "svg":
+        return <img src={fileData} alt="Something Went Wrong!" />;
+      case "pdf":
+        return (
+          <embed src={fileData} type="application/pdf" width="100%" height="500px" />
+        );
+      case "mp3":
+      case "wav":
+        return (
+          <audio controls>
+            <source src={fileData} type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        );
+      case "mp4":
+      case "webm":
+      case "ogg":
+      case "avi":
+      case "mov":
+      case "wmv":
+      case "flv":
+      case "mkv":
+      case "m4v":
+        return (
+          <video width="100%" height="auto" controls>
+            <source src={fileData} type={`video/${fileExtension}`} />
+            Your browser does not support the video tag.
+          </video>
+        );
+      case "txt":
+        return <p>{fileData}</p>;
+      case "doc":
+      case "docx":
+        return (
+          <iframe src={`https://docs.google.com/gview?url=${fileData}&embedded=true`} width="100%" height="500px" frameborder="0" scrolling="no"></iframe>
+        );
+      // Add more cases for additional file types here
+      default:
+        return <p>Preview not available for this file type.</p>;
+    }
+  };
 
-    return (
-<>
-<IconButton onClick={onOpen} bg="transparent" _hover={{background:"transparent"}} h="20px" w="20px" icon={ <BsEyeFill size="20px" />} />
+  const getFileExtension = (filename) => {
+    return filename.split(".").pop().toLowerCase();
+  };
 
-<Modal isOpen={isOpen} onClose={onClose}>
-  <ModalOverlay />
-  <ModalContent>
-    <ModalHeader>Preview</ModalHeader>
-    <ModalCloseButton />
-    <ModalBody>
+  return (
+    <>
+      <IconButton
+        onClick={onOpen}
+        bg="transparent"
+        _hover={{ background: "transparent" }}
+        h="20px"
+        w="20px"
+        icon={<BsEyeFill size="20px" />}
+      />
 
-    <img
-    src={fileData}
-    alt="Somthing Went Wrong!"
-  />
-
-    </ModalBody>
-
-    <ModalFooter>
-      <Button variant="solid" colorScheme="teal" mr={3} onClick={onClose}>
-        Close
-      </Button>
-    </ModalFooter>
-  </ModalContent>
-</Modal>
-
-</>
-)}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Preview</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{getFilePreview()}</ModalBody>
+          <ModalFooter>
+            <Button variant="solid" colorScheme="teal" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
