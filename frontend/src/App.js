432e6996d4465eb3d9dadd1e8cbbcfd69b8c9527
mukulsomukesh <mukulsomukesh@gmail.com>
2024-04-02 22:40:59 +0530

    scroll page to top when route change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,40 @@ import { Box } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer"
 import AllRoutes from './routes/AllRoutes';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
-  return (
 
+  // get page url
+  const { pathname } = useLocation();
+
+  // scroll page to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
     <>
 
-<Box bg="#f4f5f7" h="fit-content" minH={"100vh"} w="100%" fontFamily="sans-serif" >
+      {/* START: Box container */}
+      <Box bg="secondry.100" h="fit-content" minH={"100vh"} w="100%" >
 
-{/* navbar */}
-<Navbar />
+        {/* navbar */}
+        <Navbar />
 
-{/* allroutes */}
+        {/* allroutes */}
+        <Box minH={"90vh"} w="100%" fontFamily="sans-serif" >
+          <AllRoutes />
+        </Box>
 
-<Box minH={"90vh"} w="100%" fontFamily="sans-serif" >
-<AllRoutes />
-</Box>
+        {/* footer */}
+        <Footer />
 
-
-
-{/* footer */}
-<Footer />
-
-</Box>
+      </Box>
+      {/* END: Box container */}
 
     </>
-
   );
 }
 
