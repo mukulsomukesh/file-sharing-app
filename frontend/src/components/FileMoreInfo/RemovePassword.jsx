4a74c06b34fe631d6a59e50ce47149f34e7a8ce3
mukulsomukesh <mukulsomukesh@gmail.com>
2024-04-02 22:30:08 +0530

    changes UI RemovePassword.jsx

--- frontend/src/components/FileMoreInfo/RemovePassword.jsx
+++ frontend/src/components/FileMoreInfo/RemovePassword.jsx
@@ -1,20 +1,23 @@
-import { Button, Checkbox, useToast, VStack, } from "@chakra-ui/react"
+import { Button, Checkbox, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useToast, VStack, } from "@chakra-ui/react"
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { modifyFile } from "../../redux/AppReducer/action";
 import { useEffect } from "react";
+import CustomButton from "../CustomButton";
 
 export default function RemoveFilePassword({ el }) {
 
+    // define states
+    const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
     const isLoading = useSelector((state) => state.AppReducer.isLoading);
     const isError = useSelector((state) => state.AppReducer.isError);
     const message = useSelector((state) => state.AppReducer.message);
     const dispatch = useDispatch()
-    const [isFileSelected, setIsFileSelected] = useState(false);
     const toast = useToast()
 
     useEffect(() => {
 
+        // if remove password fail
         if (!isLoading && isError) {
             toast({
                 title: "Error",
@@ -25,36 +28,56 @@ export default function RemoveFilePassword({ el }) {
             });
         }
 
-
     }, [isLoading, isError])
 
 
-    function handelDeleteFile() {
-        el.isProtected= false
+    // handel remove password confirm, modal confirm button
+    function handleConfirmation() {
+        el.isProtected = false
         el.password = "";
+
+        // dispatch modify file button
         dispatch(modifyFile(el));
-      }
+
+        setIsConfirmationModalOpen(false);
+    }
+
+    // close modal, handel cancell
+    function handleCancellation() {
+        setIsConfirmationModalOpen(false);
+    }
+
 
     return (
 
         <VStack mt={5} spacing={4} align="flex-start">
-            <Checkbox
-                isChecked={isFileSelected}
-                onChange={() => { setIsFileSelected(!isFileSelected); }}
-            >
-                Remove Password from file
-            </Checkbox>
-            <Button
-                isDisabled={!isFileSelected || isLoading}
-                mt="1rem" color="white" bg="teal" w="full"
-                colorScheme="teal"
-                isLoading={isLoading}
-                loadingText={isLoading ? "Please Wait" : ""}
-                variant={isLoading ? "outline" : "solid"}
-                onClick={handelDeleteFile}
-                h="40px">
-                Remove password
-            </Button>
+
+            {/* remove password button, open modal on click */}
+            <CustomButton loadingText="Remove Password" text="Remove Password" isLoading={isLoading} onClick={() => { setIsConfirmationModalOpen(true) }} />
+
+            {/* Confirmation Modal */}
+            <Modal isOpen={isConfirmationModalOpen} onClose={handleCancellation}>
+                <ModalOverlay />
+                <ModalContent>
+
+                    {/* modaal header and content */}
+                    <ModalHeader>Confirm Deletion</ModalHeader>
+                    <ModalBody>
+                        Are you sure you want to remove this password?
+                    </ModalBody>
+
+                    <ModalFooter>
+
+                        {/* confirm remove passsword */}
+                        <Button colorScheme="red" mr={3} onClick={handleConfirmation}>
+                            Remove Password
+                        </Button>
+
+                        {/* cancel request */}
+                        <Button onClick={handleCancellation}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </VStack>
     )
 }
